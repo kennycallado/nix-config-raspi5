4450355b300dce68a9585e22109aa16be82b9c42
kennycallado <kennycallado@hotmail.com>
2024-05-16 13:53:39 +0200

    ADD guest user

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -3,14 +3,26 @@
 {
   users = {
     mutableUsers = false; # maybe true?
-    users."${host.config.user.username}" = {
-      isNormalUser = true;
-      createHome = true;
-      extraGroups = [ "wheel" "networkmanager" "disk" "video" "audio" ];
-      # password = mkIf (!host.config.known) "${host.config.user.password}";
-      hashedPassword = "${host.config.user.userHashedPassword}";
-      openssh.authorizedKeys.keys = [ "${host.config.user.sshPublicKey}" ];
+
+    users = {
+
+      root.hashedPassword = "${host.config.user.rootHashedPassword}";
+
+      "${host.config.user.username}" = {
+        isNormalUser = true;
+        createHome = true;
+        extraGroups = [ "wheel" "networkmanager" "disk" "video" "audio" ];
+        # password = mkIf (!host.config.known) "${host.config.user.password}";
+        hashedPassword = "${host.config.user.userHashedPassword}";
+        openssh.authorizedKeys.keys = [ "${host.config.user.sshPublicKey}" ];
+      };
+
+      guest = {
+        isNormalUser = true;
+        createHome = true;
+        extraGroups = [ "networkmanager" "disk" "video" "audio" ];
+        initialPassword = "guest";
+      };
     };
-    users.root.hashedPassword = "${host.config.user.rootHashedPassword}";
   };
 }
